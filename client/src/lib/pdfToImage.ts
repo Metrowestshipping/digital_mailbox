acc38be127842ce0390e6a5a3cdcc9a1cd9d239d
Metrowestshipping <UrbanSC100@gmail.com>
2026-04-09 00:54:17 -0400

    one more try

--- client/src/lib/pdfToImage.ts
+++ client/src/lib/pdfToImage.ts
@@ -21,7 +21,7 @@ export async function pdfToImage(file: File): Promise<File> {
   canvas.height = viewport.height;
 
   const ctx = canvas.getContext('2d')!;
-  await page.render({ canvasContext: ctx, viewport }).promise;
+  await page.render({ canvasContext: ctx, viewport, canvas }).promise;
 
   return new Promise((resolve, reject) => {
     canvas.toBlob(
